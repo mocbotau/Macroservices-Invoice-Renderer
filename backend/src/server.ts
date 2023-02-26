a6aa35cb44d4366d68c6651c3c2b0538c88ce13d
Daniel Cooper <z5418233@ad.unsw.edu.au>
2023-02-27 10:04:15 +1100

    Added .env for config variables

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,6 +1,11 @@
-import app from "./app";
-import config from "./config/config.json";
+import * as dotenv from "dotenv";
+dotenv.config();
 
-app.listen(config.PORT, config.HOST, () => {
-  console.log(`Invoice Rendering API started on ${config.HOST}:${config.PORT}`);
+import app from "./app";
+
+const port = parseInt(process.env.PORT, 10) || 3000;
+const host = process.env.HOST || "localhost";
+
+app.listen(port, host, () => {
+  console.log(`Invoice Rendering API started on http://${host}:${port}`);
 });
