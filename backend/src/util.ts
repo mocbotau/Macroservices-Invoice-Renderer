c15e1d3c8e0a218741bcb421e3338100c307eb22
MasterOfCubesAU <masterofcubesau@gmail.com>
2023-03-03 15:38:25 +1100

    Added more comprehensive JSDoc

--- backend/src/util.ts
+++ backend/src/util.ts
@@ -3,9 +3,9 @@ import { ValidationError, XMLParser, XMLValidator } from "fast-xml-parser";
 
 /**
  * Given a UBL formatted XML string, converts the tag/attribute into JSON key-value pairs.
- * @param {string} ublStr 
- * @throws {Error} when XML input is invalid
- * @returns {Record<string, string | number | object>}
+ * @param {string} ublStr - the UBL formatted XML string to parse
+ * @throws {Error} - when XML input is invalid
+ * @returns {Record<string, string | number | object>} - a json object representing the parsed UBL string.
  */
 export function ublToJSON(ublStr: string): Record<string, string | number | object> {
   let errObject: ValidationError;
