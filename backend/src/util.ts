8bcfacbba319acef1363b091c6aa320c1fe44d9c
MasterOfCubesAU <masterofcubesau@gmail.com>
2023-03-03 16:02:19 +1100

    Add JSONObject type

--- backend/src/util.ts
+++ backend/src/util.ts
@@ -1,13 +1,14 @@
 import { ValidationError, XMLParser, XMLValidator } from "fast-xml-parser";
+import { JSONObject } from "./interfaces";
 
 
 /**
  * Given a UBL formatted XML string, converts the tag/attribute into JSON key-value pairs.
  * @param {string} ublStr - the UBL formatted XML string to parse
  * @throws {Error} - when XML input is invalid
- * @returns {Record<string, string | number | object>} - a json object representing the parsed UBL string.
+ * @returns {JSONObject} - a json object representing the parsed UBL string.
  */
-export function ublToJSON(ublStr: string): Record<string, string | number | object> {
+export function ublToJSON(ublStr: string): JSONObject {
   let errObject: ValidationError;
   if ((errObject = (XMLValidator.validate(ublStr) as ValidationError))?.err) {
     throw Error(errObject.err.msg);
