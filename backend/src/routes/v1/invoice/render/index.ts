76d8987476466b4ac3d747fd310a740ebeb77eee
Daniel Cooper <z5418233@ad.unsw.edu.au>
2023-03-04 06:17:27 +1100

    Changed HTTP method to post

--- backend/src/routes/v1/invoice/render/index.ts
+++ backend/src/routes/v1/invoice/render/index.ts
@@ -3,8 +3,7 @@ import renderInvoiceToPDF from "@src/react/invoice";
 
 const router = express.Router();
 
-// TODO: This route is currently get for ease of testing but should later be post
-router.get("/", async (req: Request, res: Response) => {
+router.post("/", async (req: Request, res: Response) => {
   // TODO: check for output type
   // TODO: parse UBL document
   // TODO: supply arguments to renderInvoiceToPDF
