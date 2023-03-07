d4db06bf472fe3dce3997a9a217055284f8081d7
Daniel Cooper <z5418233@ad.unsw.edu.au>
2023-03-08 07:17:52 +1100

    Revert "F14AMACROS-27: Test change to test webhook"
    This reverts commit 79b84be9834e007ed38cf31a83aa32c10b50ffb3.

--- backend/src/routes/v1/healthcheck/index.ts
+++ backend/src/routes/v1/healthcheck/index.ts
@@ -4,7 +4,7 @@ import express from "express";
 const router = express.Router();
 
 router.get("/", (req: Request, res: Response) => {
-  res.json({ "status": "Running!", "test": "test" });
+  res.json({ "status": "Running!" });
 });
 
 export default router;
