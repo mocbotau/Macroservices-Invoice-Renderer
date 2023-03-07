79b84be9834e007ed38cf31a83aa32c10b50ffb3
Daniel Cooper <z5418233@ad.unsw.edu.au>
2023-03-08 07:16:24 +1100

    F14AMACROS-27: Test change to test webhook

--- backend/src/routes/v1/healthcheck/index.ts
+++ backend/src/routes/v1/healthcheck/index.ts
@@ -4,7 +4,7 @@ import express from "express";
 const router = express.Router();
 
 router.get("/", (req: Request, res: Response) => {
-  res.json({ "status": "Running!" });
+  res.json({ "status": "Running!", "test": "test" });
 });
 
 export default router;
