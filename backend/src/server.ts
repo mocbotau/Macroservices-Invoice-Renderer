d24584aaa9ab2b7e8a09c4cbbb45666fcdd8f05f
MasterOfCubesAU <masterofcubesau@gmail.com>
2023-03-15 16:03:25 +1100

    Added v2 docs

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -16,11 +16,28 @@ const swaggerOptions = {
   customSiteTitle: "MACROSERVICES",
 };
 
-app.use(
-  "/docs",
-  swaggerUi.serve,
-  swaggerUi.setup(YAML.load("docs/api.yml"), swaggerOptions)
-);
+const v1JSON = YAML.load("docs/v1.yml");
+const v2JSON = YAML.load("docs/v2.yml");
+
+const v1HTML = swaggerUi.generateHTML(v1JSON, swaggerOptions);
+const v2HTML = swaggerUi.generateHTML(v2JSON, swaggerOptions);
+
+// v1 docs
+app.use("/docs/v1", swaggerUi.serveFiles(v1JSON, swaggerOptions));
+app.get("/docs/v1", (req, res) => {
+  res.send(v1HTML);
+});
+
+// v2 docs
+app.use("/docs/v2", swaggerUi.serveFiles(v2JSON, swaggerOptions));
+app.get("/docs/v2", (req, res) => {
+  res.send(v2HTML);
+});
+
+// Redirect /docs to latest
+app.get("/docs", (req, res) => {
+  res.redirect("/docs/v2");
+});
 
 app.listen(port, host, () => {
   logger.info(`Invoice Rendering API started on http://${host}:${port}`);
