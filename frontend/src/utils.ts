11905dba4260503a4d89f638e2048a284b512e21
Daniel Cooper <z5418233@ad.unsw.edu.au>
2023-03-25 10:14:33 +1100

    Implemented basic file upload

---
+++ frontend/src/utils.ts
@@ -0,0 +1,53 @@
+/**
+ * Prompts the user to upload a file
+ *
+ * @param {string} fileType - file type to upload (eg ".csv")
+ * @returns {Promise<File>} - uploaded file
+ */
+export async function uploadFile(fileType: string): Promise<File> {
+  return new Promise((res) => {
+    const input = document.createElement("input");
+    input.type = "file";
+    input.accept = fileType;
+
+    input.onchange = (e) => {
+      if (!e || !e.target) {
+        return;
+      }
+      const target = e.target as HTMLInputElement;
+      if (!target.files) {
+        return;
+      }
+      const file = target.files[0];
+
+      res(file);
+    };
+
+    input.click();
+  });
+}
+
+/**
+ * Reads a file to text
+ *
+ * @param {File} file - file to read
+ * @returns {Promise<string>} - string that is read
+ */
+export async function readFileAsText(file: File): Promise<string> {
+  return new Promise((res, rej) => {
+    const reader = new FileReader();
+
+    reader.onload = (evt) => {
+      if (!evt.target) return;
+      if (evt.target.readyState != 2) return;
+      if (evt.target.error) {
+        rej(evt.target.error);
+        return;
+      }
+
+      res(evt.target.result as string);
+    };
+
+    reader.readAsText(file);
+  });
+}
---
+++ frontend/tests/pages/editor.test.tsx
@@ -0,0 +1,42 @@
+import { render, screen } from "@testing-library/react";
+import Editor from "@src/pages/editor";
+import userEvent from "@testing-library/user-event";
+
+const mockUploadFile = jest.fn();
+
+jest.mock("@src/utils", () => {
+  const orig = jest.requireActual("@src/utils");
+
+  return {
+    __esModule: true,
+    ...orig,
+    uploadFile: (fileType: string) => mockUploadFile(fileType),
+  };
+});
+
+beforeEach(() => {
+  jest.restoreAllMocks();
+
+  mockUploadFile.mockClear();
+});
+
+describe("Editor", () => {
+  describe("Upload Screen", () => {
+    it("uploads a file when clicking the button", async () => {
+      mockUploadFile.mockImplementation(() =>
+        Promise.resolve(new File(["hello,there"], "test.csv"))
+      );
+
+      render(<Editor />);
+
+      const button = screen.getByRole("button", {
+        name: /upload csv file/i,
+      });
+      expect(button).toBeTruthy();
+
+      await userEvent.click(button);
+
+      expect(mockUploadFile).toHaveBeenCalled();
+    });
+  });
+});
