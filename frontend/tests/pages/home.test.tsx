696f3ac640714f1a1887a4d3dcbd0c59515185a4
MasterOfCubesAU <masterofcubesau@gmail.com>
2023-03-26 13:58:47 +1100

    Added middleware tests

--- frontend/tests/pages/home.test.tsx
+++ frontend/tests/pages/home.test.tsx
@@ -1,40 +1,11 @@
-import { render, screen } from "@testing-library/react";
 import "@testing-library/jest-dom/extend-expect";
-import Home, { serverSideProps } from "@src/pages/index";
-import mockRouter from "next-router-mock";
-
-const mockedRouter = {
-  push: jest.fn(),
-};
-jest.mock("next/router", () => ({
-  ...jest.requireActual("next/router"),
-  useRouter: () => mockedRouter,
-}));
+import { serverSideProps } from "@src/pages/index";
 
 const VALID_SESSION = {
   email: "valid@email.com",
 };
 
-beforeAll(() => {
-  jest.clearAllMocks();
-});
-
 describe("Home", () => {
-  test("/ redirects to /login (if not signed in)", () => {
-    render(<Home />);
-    mockedRouter.push.mockImplementationOnce((url) => {
-      expect(url).toBe("/login");
-    });
-  });
-
-  test("/ redirects to /editor (if signed in)", () => {
-    mockRouter.push("/");
-    render(<Home user={VALID_SESSION} />);
-
-    mockedRouter.push.mockImplementationOnce((url) => {
-      expect(url).toBe("/editor");
-    });
-  });
   test("getServerSideProps with valid user", async () => {
     expect(await serverSideProps(VALID_SESSION)).toEqual({
       props: { user: VALID_SESSION },
